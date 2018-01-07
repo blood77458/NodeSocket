ddb8ab0a4bc3d2d773ea30b04cc8eeea77fbf381
blood77458 <blood77458@outlook.com>
2018-01-07 12:47:26 +0800

    bug

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index1.html', { title: 'Express' });
+  res.render('Index1.html', { title: 'Express' });
 });
 
 module.exports = router;
